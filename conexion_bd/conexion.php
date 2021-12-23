c5fe900bdbba0955c53408558ba232200972736a
Sdraen <93408900+Sdraen@users.noreply.github.com>
2021-12-23 03:34:03 -0300

    Update conexion.php

--- conexion_bd/conexion.php
+++ conexion_bd/conexion.php
@@ -1,17 +1,14 @@
 <?php
-
 try{
-
     //Credenciales para la conexion a phpmyadmin
-    $host = "localhost";
-    $dbname= "id17945216_datos5";
-    $username= "id17945216_grupo5";
-    $password = "T/wXDI73!3+o&S<4";
-    //Creamos un nuevo objeto PDO para crear una conexion a mysql
-    $conn = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
+    $host = "mysql.face.ubiobio.cl";
+    $dbname= "5soft2021";
+    $username= "g5soft";
+    $password = "g5isw2021";
 
-}catch(PDOException $e){
-    die("No se ha podido conectar la base de datos".$e->getMessage());
+    $conn = new mysqli($host,$username,$password,$dbname);
+
+}catch(Exception $e){
+    echo "Error: ".$e->getMessage();
 }
-
 ?>
